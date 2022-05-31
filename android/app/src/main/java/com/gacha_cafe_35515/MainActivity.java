9512a1710610d458b966b339d280c9f49e678aed
Crowdbotics <team@crowdbotics.com>
2022-05-31 01:54:35 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/gacha_cafe_35515/MainActivity.java
@@ -0,0 +1,15 @@
+package com.gacha_cafe_35515;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "gacha_cafe_35515";
+  }
+}
